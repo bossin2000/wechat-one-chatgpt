dd85f380c8ccf670596d9a6892480c528bed61c6
bossin2000 <129158918+bossin2000@users.noreply.github.com>
2023-03-28 16:52:18 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# encoding:utf-8
+
+import config
+from channel import channel_factory
+from common.log import logger
+
+from plugins import *
+if __name__ == '__main__':
+    try:
+        # load config
+        config.load_config()
+
+        # create channel
+        channel_name='wx'
+        channel = channel_factory.create_channel(channel_name)
+        if channel_name=='wx':
+            PluginManager().load_plugins()
+
+        # startup channel
+        channel.startup()
+    except Exception as e:
+        logger.error("App startup failed!")
+        logger.exception(e)
